48cb913fc42e965cdb9dfbd4e90a4c6f1b7268fd
Tomáš Pružina <pruzinat@gmail.com>
2014-05-02 00:24:18 +0200

    4/6 cleanup in md5/md5.h (removed exporting private functions) 	modified:   md5/md5.h

--- md5/md5.h
+++ md5/md5.h
@@ -5,14 +5,9 @@
 
 #include "../common.h"
 
-// these calculate raw hash into buffer 'hash'
-int md5_calculate_hash(const char *filename, unsigned char hash[MD5_DIGEST_LENGTH]);
-int md5_calculate_hash_from_string(const char *string, unsigned char hash[MD5_DIGEST_LENGTH]);
-
 // return nice hash string in hexadecimal (same as md5sum)
 char *md5_sanitized_hash_of_file(char *file_path);
 char *md5_sanitized_hash_of_string(char *string);
 
-char *md5_sanitized_hash (unsigned char hash[MD5_DIGEST_LENGTH]);
 
 #endif
